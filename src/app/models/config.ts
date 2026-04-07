826bf6a4e60691e654c09df6cacfd42509774a4a
Robert <rartesona@uoc.edu>
2026-04-07 11:35:28 +0200

    app espaiflex 07-04-2026

--- src/app/models/config.ts
+++ src/app/models/config.ts
@@ -2,7 +2,8 @@ const hostname = window.location.hostname;
 export const config = {
   title: 'EspaiFlex',
   url_web: 'http://168.231.77.239/apis/',
-  url: (hostname == 'localhost'?'http://localhost:8000/api/':'http://168.231.77.239:8000/api/'),
+  url: 'http://168.231.77.239/api/',
+  // url: (hostname == 'localhost'?'http://localhost:8000/api/':'http://168.231.77.239:8000/api/'),
   url_foto: 'assets/upload/',
   url_upload: 'http://localhost:3000/upload',
   token: '1|hjMb2oAV6XZuofxOnfSg8Ti5FEvWViONXTtjEhu0fb131ad5'
